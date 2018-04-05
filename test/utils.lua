930b14e1e765db8b8c39208f4c2e42b6a28602f6
Alexander Turenko <alexander.turenko@tarantool.org>
2018-04-05 20:12:46 +0300

    Fix indent in test/utils.lua

--- test/utils.lua
+++ test/utils.lua
@@ -6,7 +6,7 @@ local utils = {}
 
 function utils.format_result(name, query, variables, result)
     return ('RUN %s {{{\nQUERY\n%s\nVARIABLES\n%s\nRESULT\n%s\n}}}\n'):format(
-    name, query:rstrip(), yaml.encode(variables), yaml.encode(result))
+        name, query:rstrip(), yaml.encode(variables), yaml.encode(result))
 end
 
 function utils.print_and_return(...)
